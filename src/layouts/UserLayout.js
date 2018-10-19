87112d7c495c5ace09c4098e57b672f97db6d60d
xkdzy2002 <107829417@qq.com>
2018-10-19 20:47:30 +0800

    完善UI

--- src/layouts/UserLayout.js
+++ src/layouts/UserLayout.js
@@ -4,7 +4,8 @@ import DocumentTitle from 'react-document-title';
 import { Icon } from 'antd';
 import GlobalFooter from '../components/GlobalFooter';
 import styles from './UserLayout.less';
-import logo from '../assets/logo.svg';
+// import logo from '../assets/logo.svg'
+import logo from '../assets/logo.png';
 import { getRoutes, getPageQuery, getQueryPath } from '../utils/utils';
 
 const links = [
@@ -27,7 +28,7 @@ const links = [
 
 const copyright = (
   <Fragment>
-    Copyright <Icon type="copyright" /> 2018 蚂蚁金服体验技术部出品
+    Copyright <Icon type="copyright" /> 2018 中国联通学院
   </Fragment>
 );
 
@@ -60,10 +61,10 @@ class UserLayout extends React.PureComponent {
               <div className={styles.header}>
                 <Link to="/">
                   <img alt="logo" className={styles.logo} src={logo} />
-                  <span className={styles.title}>Ant Design</span>
+                  <span className={styles.title}>王卡驿站</span>
                 </Link>
               </div>
-              <div className={styles.desc}>Ant Design 是西湖区最具影响力的 Web 设计规范</div>
+              {/*<div className={styles.desc}>Ant Design 是西湖区最具影响力的 Web 设计规范</div>*/}
             </div>
             <Switch>
               {getRoutes(match.path, routerData).map(item => (
