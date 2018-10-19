2f39b66ac698aa695ddbd726e259da6eddf387bd
秦璇-培训忻州 <qinx6@chinaunicom.cn>
2018-10-19 17:24:24 +0800

    修复刷新bug

---
+++ src/routes/Qinx/Test.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class Test extends Component {
+  render() {
+    return <div style={{ height: '100%', backgroundColor: 'red' }} />;
+  }
+}
