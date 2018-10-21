3d002e2b47705a21c51b9274d450e53d71995a4d
xkdzy2002 <107829417@qq.com>
2018-10-21 15:04:50 +0800

    增强数据分析功能，动态变化效果

--- src/axios/index.js
+++ src/axios/index.js
@@ -20,11 +20,15 @@ export default class Axios {
     });
   }
 
-  static ajax(options) {
+  static ajax(options){
     let loading;
 
+    // MOCK测试API的BaseURL用于在无后端数据库服务器下的测试
+    let baseApi = 'https://easy-mock.com/mock/5bcbe3f1e8326e1cdcb850e3/api';
+
     // let baseApi = 'https://www.easy-mock.com/mock/5bc303ecb0bf35423bd75308/mockapi';
-    let baseApi = 'http://10.52.200.46:9002/api';
+    // let baseApi = 'http://10.52.200.46:9002/api';
+
     return new Promise((resolve, reject) => {
       axios({
         url: options.url,
@@ -37,10 +41,12 @@ export default class Axios {
         if(response.status == '200'){
             console.log(response.data);
             let res = response.data;
-            Modal.info({
-              title: '获取数据提示',
-              content: response.error == null?'数据正常返回': response.error
-            });
+
+            // 如果是是使用Mock测试数据需要屏蔽下面的，否则功能不正常
+            // Modal.info({
+            //   title: '获取数据提示',
+            //   content: response.error == null?'数据正常返回': response.error
+            // });
 
             resolve(res);
         }else{
